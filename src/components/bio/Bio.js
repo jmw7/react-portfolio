03fda75917f4f95a7a861effb0b93fe0b785116d
mendolawong <jwong.ncsu@gmail.com>
2020-05-08 10:42:31 -0400

    Updated bio

--- src/components/bio/Bio.js
+++ src/components/bio/Bio.js
@@ -11,13 +11,13 @@ export default function Bio() {
         <img src={Travel} alt='New Zealand' className='bio-pic' />
         <div className="bio">
           <p>
-          I'm a full-stack web developer focused on React, Node, and PostgreSQL, sprinkled with a little bit of ASP Classic and SQL Server.
+          I'm a full-stack web developer focused on React, Node, and PostgreSQL, sprinkled with a little bit of Classic ASP and SQL Server.
           <br /><br />
           I love web development because it answers a myriad of problems in the world, with a little bit of tailoring of course.
           It connects people in ways that weren't possible before.
           I've taken this coding journey solo for quite some time and I'm thirsty to contribute and be challenged by a team of great developers.
           <br /><br />
-          A few other things that get me out of bed in the morning - I am an avid hiker, an out of habit reader, and I'm pretty good at Tetris too. 
+          A few other things that get me out of bed in the morning - I love to hike and get outdoors, I am an out of habit reader, and I'm pretty good at Tetris too. 
           </p>
         </div>
       </div>
